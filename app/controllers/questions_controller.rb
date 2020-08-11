b11675bf0bafa1c13bf69c338cc72f39f31bf5f5
Brian Burrow <bburrow81@gmail.com>
2020-08-11 21:22:41 +0200

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,14 @@
 class QuestionsController < ApplicationController
   def ask; end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+        @answer = "great"
+    elsif @question.match(/[?\s]\z/)
+      @answer = "Silly question, get dressed and go to work!."
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
