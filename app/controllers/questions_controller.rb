fd4570a11e83161a839cc22e0a2ea5bd8f55580f
Brian Burrow <bburrow81@gmail.com>
2020-08-13 17:29:37 +0200

    Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,10 +3,10 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    if @question == "I am going to work"
-        @answer = "great"
-    elsif @question.match(/[?\s]\z/)
-      @answer = "Silly question, get dressed and go to work!."
+    if @question == 'I am going to work'
+      @answer = 'great'
+    elsif @question.include? '?' #match(/[?\s]\z/)
+      @answer = 'Silly question, get dressed and go to work!.'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
